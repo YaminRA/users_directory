9f83a98500ccbd554844fcb199c2a9c68a4206cd
Yamin Ruiz Aparicio <yamin.raparicio@gmail.com>
2019-06-06 14:18:26 -0500

    Deployment to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBEUOrbCukCqyUIyX3fobt60qAhAbc45SE',
+    authDomain: 'user-directory-39176.firebaseapp.com',
+    databaseURL: 'https://user-directory-39176.firebaseio.com',
+    projectId: 'user-directory-39176',
+    storageBucket: 'user-directory-39176.appspot.com',
+    messagingSenderId: '81465758659',
+    appId: '1:81465758659:web:3e118f1718e52e5c'
+  }
 };
